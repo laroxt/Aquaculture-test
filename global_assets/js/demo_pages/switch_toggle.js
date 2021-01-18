caa7a4e004913d9f048a2ca18d3a626b1d73f62c
Cyprian <cypchg@gmail.com>
2021-01-18 08:56:29 +0800

    second commit

---
+++ global_assets/js/demo_pages/switch_toggle.js
@@ -0,0 +1,13 @@
+function toggle_fv(user, ctrl) {
+  ctrl.setAttribute("disabled", true);
+  setTimeout(function (user, ctrl) {
+    window.location.href = "./superadmin-nm-nc-management-net-nv-overview.html";
+  }, 1000);
+}
+
+function toggle_nv(user, ctrl) {
+  ctrl.setAttribute("disabled", true);
+  setTimeout(function (user, ctrl) {
+    window.location.href = "./superadmin-nm-nc-management-net-fv-overview.html";
+  }, 1000);
+}
